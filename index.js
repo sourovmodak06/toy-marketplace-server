14db67dcb100877b7e24ea36c22542e4993fd71f
sourovmodak06 <sourovmodak650@gmail.com>
2023-05-18 17:59:19 +0600

    server side created

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    res.send("Super Hero Toy Store server is running");
+})
+
+app.listen(port, () => {
+    console.log(`Super Hero Toy Store server PORT: ${port}`);
+})
+
