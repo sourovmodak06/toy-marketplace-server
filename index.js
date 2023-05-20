1d063c831ee40e1d0812ce1cbfcf4adec8b51f92
sourovmodak06 <sourovmodak650@gmail.com>
2023-05-20 17:40:48 +0600

    Add toy Category DB added

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
-
 
 // middleware
 app.use(cors());
 app.use(express.json());
-
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zhzphwv.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -20,48 +16,73 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     await client.connect();
 
+    const toyCollection = client.db("ToyMarketplace").collection("gallery");
+    const popularProducts = client
+      .db("ToyMarketplace")
+      .collection("PopularProducts");
+    const marvelCategory = client
+      .db("ToyMarketplace")
+      .collection("MarvelCategory");
+    const starWarCategory = client
+      .db("ToyMarketplace")
+      .collection("starWarCategory");
+    const transformerCategory = client
+      .db("ToyMarketplace")
+      .collection("transformerCategory");
+    const dcCategory = client.db("ToyMarketplace").collection("dcCategory");
 
-    const toyCollection = client.db('ToyMarketplace').collection("gallery");
-    const popularProducts = client.db('ToyMarketplace').collection("PopularProducts");
-    const marvelCategory = client.db('ToyMarketplace').collection("MarvelCategory");
-    const starWarCategory = client.db('ToyMarketplace').collection("starWarCategory");
-    const transformerCategory = client.db('ToyMarketplace').collection("transformerCategory");
-    const dcCategory = client.db('ToyMarketplace').collection("dcCategory");
+    const addToy = client.db("ToyMarketplace").collection("addToy");
+
+    app.get("/gallery", async (req, res) => {
+      const result = await toyCollection.find().toArray();
+      res.send(result);
+    });
+    app.get("/products", async (req, res) => {
+      const result = await popularProducts.find().toArray();
+      res.send(result);
+    });
+    app.get("/marvelCategory", async (req, res) => {
+      const result = await marvelCategory.find().toArray();
+      res.send(result);
+    });
+    app.get("/starWarCategory", async (req, res) => {
+      const result = await starWarCategory.find().toArray();
+      res.send(result);
+    });
+    app.get("/transformerCategory", async (req, res) => {
+      const result = await transformerCategory.find().toArray();
+      res.send(result);
+    });
+    app.get("/dcCategory", async (req, res) => {
+      const result = await dcCategory.find().toArray();
+      res.send(result);
+    });
+
+    app.post("/addToy", async (req, res) => {
+      const newToy = req.body;
+      const result = await addToy.insertOne(newToy);
+      res.send(result);
+    });
+    app.get("/addToy", async (req, res) => {
+      const cursor = addToy.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    app.get("/addToy/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await addToy.findOne(query);
+      res.send(result);
+    });
 
 
-
-
-    app.get('/gallery', async(req, res) => {
-        const result = await toyCollection.find().toArray();
-        res.send(result);
-    })
-    app.get('/products', async(req, res) => {
-        const result = await popularProducts.find().toArray();
-        res.send(result);
-    })
-    app.get('/marvelCategory', async(req, res) => {
-        const result = await marvelCategory.find().toArray();
-        res.send(result);
-    })
-    app.get('/starWarCategory', async(req, res) => {
-        const result = await starWarCategory.find().toArray();
-        res.send(result);
-    })
-    app.get('/transformerCategory', async(req, res) => {
-        const result = await transformerCategory.find().toArray();
-        res.send(result);
-    })
-    app.get('/dcCategory', async(req, res) => {
-        const result = await dcCategory.find().toArray();
-        res.send(result);
-    })
 
 
     await client.db("admin").command({ ping: 1 });
@@ -73,13 +94,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-app.get('/', (req, res) => {
-    res.send("Super Hero Toy Store server is running");
-})
+app.get("/", (req, res) => {
+  res.send("Super Hero Toy Store server is running");
+});
 
 app.listen(port, () => {
-    console.log(`Super Hero Toy Store server PORT: ${port}`);
-})
-
+  console.log(`Super Hero Toy Store server PORT: ${port}`);
+});
