fa16fe52b960b60a22b476fd1e2db4d719e6c51c
sourovmodak06 <sourovmodak650@gmail.com>
2023-05-21 19:31:18 +0600

    server side update

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
 
     const toyCollection = client.db("ToyMarketplace").collection("gallery");
     const popularProducts = client
