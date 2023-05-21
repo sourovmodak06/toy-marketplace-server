a92a9b5db0d593b17f84b3d07abb83a9c4db19d4
sourovmodak06 <sourovmodak650@gmail.com>
2023-05-21 12:05:15 +0600

    deleted dbcreated

--- index.js
+++ index.js
@@ -92,6 +92,13 @@ async function run() {
 
 
 
+    app.delete('/toy/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await addToy.deleteOne(query)
+      res.send(result)
+    })
+
 
 
     await client.db("admin").command({ ping: 1 });
